b1a88f9de094b25a642917a504cca993806a2a2c
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-04 01:24:40 +0200

    update response stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { IProduct } from './models/product'
 import { products as data } from './data/products'
 import { ProductsService } from './services/products.services'
+import { Observable, tap } from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -10,16 +11,22 @@ import { ProductsService } from './services/products.services'
 })
 export class AppComponent implements OnInit {
   title = 'angular app'
-  products: IProduct[] = []
+  // products: IProduct[] = []
+  products$: Observable<IProduct[]>
   loading = false
 
   constructor (private productsService: ProductsService) {}
 
   ngOnInit (): void {
     this.loading = true
-    this.productsService.getAll().subscribe(products => {
-      this.products = products
-      this.loading = false
-    })
+    this.products$ = this.productsService
+      .getAll()
+      .pipe(
+        tap(() => this.loading = false)
+        )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
   }
 }
