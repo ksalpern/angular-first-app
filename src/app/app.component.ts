bf5b5b2384f1410ef2d754b012cb70518c157b0a
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-04 01:07:34 +0200

    get products from the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { IProduct } from './models/product'
 import { products as data } from './data/products'
+import { ProductsService } from './services/products.services'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular app'
-  products: IProduct[] = data
+  products: IProduct[] = []
+
+  constructor (private productsService: ProductsService) {}
+
+  ngOnInit (): void {
+    this.productsService.getAll().subscribe(products => {
+      this.products = products
+    })
+  }
 }
