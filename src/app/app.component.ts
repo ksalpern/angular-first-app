3c77f04d3dfaeed8ffe7dcfed9a94f9c6dbf414f
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-04 21:39:48 +0200

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,8 @@
-import { Component, OnInit } from '@angular/core'
-import { IProduct } from './models/product'
-import { products as data } from './data/products'
-import { ProductsService } from './services/products.services'
-import { Observable, tap } from 'rxjs'
-import { ModalService } from './services/modal.service'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'angular app'
-  // products: IProduct[] = []
-  // products$: Observable<IProduct[]>
-  loading = false
-  term = ''
-
-  constructor (
-    public productsService: ProductsService,
-    public modalService: ModalService
-  ) {}
-
-  ngOnInit (): void {
-    this.loading = true
-    // this.products$ = this.productsService
-    //   .getAll()
-    //   .pipe(tap(() => (this.loading = false)))
-    this.productsService.getAll().subscribe(() => {
-      // this.products = products
-      this.loading = false
-    })
-  }
-}
+export class AppComponent {}
--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ModalService } from 'src/app/services/modal.service'
+import { ProductsService } from 'src/app/services/products.services'
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
+  title = 'angular app'
+  // products: IProduct[] = []
+  // products$: Observable<IProduct[]>
+  loading = false
+  term = ''
 
+  constructor (
+    public productsService: ProductsService,
+    public modalService: ModalService
+  ) {}
+
+  ngOnInit (): void {
+    this.loading = true
+    // this.products$ = this.productsService
+    //   .getAll()
+    //   .pipe(tap(() => (this.loading = false)))
+    this.productsService.getAll().subscribe(() => {
+      // this.products = products
+      this.loading = false
+    })
+  }
 }
